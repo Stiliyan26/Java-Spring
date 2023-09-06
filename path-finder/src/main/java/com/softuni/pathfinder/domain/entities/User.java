3d0d955b9614df2370a074caf30fd8eee4f28f31
Stiliyan26 <stili262002@abv.bg>
2023-09-06 18:09:46 +0300

    path-finder init

---
+++ path-finder/src/main/java/com/softuni/pathfinder/domain/entities/User.java
@@ -0,0 +1,98 @@
+package com.softuni.pathfinder.domain.entities;
+
+import com.softuni.pathfinder.domain.enums.Level;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {CascadeType.MERGE, CascadeType.DETACH})
+    private Set<Role> roles;
+
+    @Enumerated(EnumType.STRING)
+    private Level level;
+
+    @Column
+    private String fullName;
+
+    @Column
+    private Integer age;
+
+    public User() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public User setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public User setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public User setRoles(Set<Role> roles) {
+        this.roles = roles;
+        return this;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public User setLevel(Level level) {
+        this.level = level;
+        return this;
+    }
+}
