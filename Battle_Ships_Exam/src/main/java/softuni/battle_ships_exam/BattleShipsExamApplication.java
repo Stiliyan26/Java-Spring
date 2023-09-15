6e5354d72cea7eebc6c66cc476abef56f44f8aa4
Stiliyan26 <stili262002@abv.bg>
2023-09-15 16:56:27 +0300

    Battle Ships exam init

---
+++ Battle_Ships_Exam/src/main/java/softuni/battle_ships_exam/BattleShipsExamApplication.java
@@ -0,0 +1,13 @@
+package softuni.battle_ships_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleShipsExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleShipsExamApplication.class, args);
+    }
+
+}
---
+++ Battle_Ships_Exam/src/test/java/softuni/battle_ships_exam/BattleShipsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.battle_ships_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleShipsExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
